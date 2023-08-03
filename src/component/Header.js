a0ef63caaf99660a8f2568df72260172c35c0e9c
mfaisalakram <faisalakram1133@gmail.com>
2023-08-03 00:19:45 -0700

    tabs initial fix

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,15 +2,13 @@ import React from "react";
 import mainImage from "../assets/main.png";
 const Header = () => {
   return (
-    <header className="p-4 flex items-center justify-between border shadow-sm">
-      <div className="flex items-center">
-        <img src={mainImage} alt="Logo" className="w-12 h-12 mr-4" />
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-white rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-300"
-        />
-      </div>
+    <header className="p-4 flex items-center justify-between border-b">
+      <img src={mainImage} alt="Logo" className="w-70 h-8 mr-4" />
+      <input
+        type="text"
+        placeholder="Search"
+        className="bg-white rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-300"
+      />
 
       <button className="text-white p-2">
         <svg
--- src/component/Tab.js
+++ src/component/Tab.js
@@ -2,17 +2,27 @@ import React from "react";
 
 const Tab = () => {
   return (
-    <div className="flex justify-center items-center border-b border-gray-400">
-      <div className="p-4 cursor-pointer border-b-2 border-transparent hover:border-black">
-        Stays
+    <>
+      <div className="flex justify-center items-center border-b border-gray-400">
+        <div className="p-4 cursor-pointer border-b-2 border-transparent hover:border-black">
+          Stays
+        </div>
+        <div className="p-4 cursor-pointer border-b-2 border-transparent hover:border-black border-black">
+          Experiences
+        </div>
+        <div className="p-4 cursor-pointer border-b-2 border-transparent hover:border-black">
+          Online Experiences
+        </div>
       </div>
-      <div className="p-4 cursor-pointer border-b-2 border-transparent hover:border-black border-black">
-        Experiences
+      {/* // */}
+      {/* <div className="flex items-center justify-center h-48 bg-gray-100 rounded-full"> */}
+      <div className="flex items-center justify-center h-18 w-13 border border-gray-300 rounded-full">
+        <button className="px-4 py-2 border-r border-gray-300">Date</button>
+        <button className="px-4 py-2 border-r border-gray-300">Who</button>
+        <button className="px-4 py-2">Search destinations</button>
       </div>
-      <div className="p-4 cursor-pointer border-b-2 border-transparent hover:border-black">
-        Online Experiences
-      </div>
-    </div>
+      {/* </div> */}
+    </>
   );
 };
 
